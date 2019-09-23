7c409208b96435ee6af7e23e3c55e966185bc668
Arshpreet1705 <54538308+Arshpreet1705@users.noreply.github.com>
2019-09-23 18:42:55 +0530

    Add files via upload

---
+++ GreatOutdoor/GreatOutdoor.Entities/Product.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capgemini.GreatOutdoor.Entities
+{
+    /// <summary>
+    /// Interface for Product Entity
+    /// </summary>    
+    public interface IProduct
+    {
+        Guid ProductID { get; set; }
+        string ProductCategory { get; set; }
+        string ProductDescription { get; set; }
+        int ProductQuantity { get; set; }
+        double ProductPrice { get; set; }
+        DateTime CreationDateTime { get; set; }
+        DateTime LastModifiedDateTime { get; set; }
+    }
+    /// <summary>
+    /// Represents Product
+    /// </summary>
+
+    public class Product:IProduct
+    {
+        /* Auto-Implemented Properties*/
+        [Required("Supplier ID can't be blank")]
+        public Guid ProductID { get; set; }
+
+        public string ProductCategory { get; set; }
+
+        public string ProductDescription
+
+           
+        /* Constructor */
+        public Product()
+        {
+            ProductID = default(Guid);
+            ProductCategory = null;
+
+        }
+
+
+
+    }
+}
